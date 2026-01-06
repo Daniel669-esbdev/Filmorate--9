03a4dc62772f0a6676a60298bfdfa7f0bf2cde96
Daniel <dzdaniel669@gmail.com>
2026-01-06 16:45:28 +0500

    spr10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    @PositiveOrZero
+    private long id;
+
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Описание не может превышать 200 символов")
+    private String description;
+
+    @NotNull(message = "Дата релиза не может быть null")
+    @PastOrPresent(message = "Дата релиза не может быть в будущем")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность должна быть")
+    private long duration;
 }
