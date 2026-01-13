c6c4949c03b85531a6c6e3c003d0cd838749da44
Daniel <dzdaniel669@gmail.com>
2026-01-13 12:20:10 +0500

    spr12

---
+++ src/main/resources/data.sql
@@ -0,0 +1,43 @@
+INSERT INTO mpa (id, name)
+SELECT 1, 'G' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 1);
+
+INSERT INTO mpa (id, name)
+SELECT 2, 'PG' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 2);
+
+INSERT INTO mpa (id, name)
+SELECT 3, 'PG-13' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 3);
+
+INSERT INTO mpa (id, name)
+SELECT 4, 'R' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 4);
+
+INSERT INTO mpa (id, name)
+SELECT 5, 'NC-17' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 5);
+
+INSERT INTO genres (id, name)
+SELECT 1, 'Комедия' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 1);
+
+INSERT INTO genres (id, name)
+SELECT 2, 'Драма' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 2);
+
+INSERT INTO genres (id, name)
+SELECT 3, 'Мультфильм' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 3);
+
+INSERT INTO genres (id, name)
+SELECT 4, 'Триллер' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 4);
+
+INSERT INTO genres (id, name)
+SELECT 5, 'Документальный' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 5);
+
+INSERT INTO genres (id, name)
+SELECT 6, 'Боевик' FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 6);
