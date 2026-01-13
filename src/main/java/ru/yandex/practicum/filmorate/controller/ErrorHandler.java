f1c518f7e9813d0e6434aa8741f124a6ee1bd5eb
Daniel <dzdaniel669@gmail.com>
2026-01-13 12:09:33 +0500

    spr12

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import lombok.extern.slf4j.Slf4j;
+
 
 
 import java.util.stream.Collectors;
