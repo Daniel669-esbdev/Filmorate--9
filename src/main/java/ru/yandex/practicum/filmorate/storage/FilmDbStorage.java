38b5e5ce1a7762802722f77de8e7e8169675fdc9
Kamil Medjidov <captain-kam@yandex.ru>
2026-01-19 19:38:20 +0400

    Checkstyle: не убрал неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.extern.slf4j.Slf4j;
-import org.h2.jdbc.JdbcDatabaseMetaData;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
