25172d39d39c8f99ed2349cb910d5cae978d754f
Daniel <dzdaniel669@gmail.com>
2026-01-19 11:37:01 +0500

    reviews

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ReviewStorage {
+
     Review create(Review review);
+
     Review update(Review review);
+
     boolean delete(Long reviewId);
+
     Optional<Review> findById(Long id);
+
     List<Review> findByFilmId(Long filmId, Integer count);
 }
