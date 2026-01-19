d013a0b8dbc0362f9ad600312494421cc59c67e1
Daniel <dzdaniel669@gmail.com>
2026-01-19 22:03:40 +0500

    reviews

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -3,27 +3,22 @@ package ru.yandex.practicum.filmorate.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review {
-
     @JsonProperty("reviewId")
     private Long id;
 
     @NotNull(message = "filmId обязателен")
-    @Positive(message = "filmId должен быть положительным")
     private Long filmId;
 
     @NotNull(message = "userId обязателен")
-    @Positive(message = "userId должен быть положительным")
     private Long userId;
 
     @NotBlank(message = "Содержимое отзыва не может быть пустым")
