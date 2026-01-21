3cfddad2d37396336bd9bb0ab182c63357d738f8
Evgeniy <marchinskiye22@mail.ru>
2026-01-22 02:44:28 +0700

    a

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public interface EventStorage {
     Event create(Event event);
+
     List<Event> findByUserId(Long userId);
+
     void deleteByUserId(Long userId);
+
     void deleteAll();
 }
