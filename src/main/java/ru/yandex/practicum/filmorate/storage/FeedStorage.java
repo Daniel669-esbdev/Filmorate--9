9908c89a88cc92bdca728af2d70f4a8d261b6687
Daniel <dzdaniel669@gmail.com>
2026-01-22 19:53:09 +0500

    recomm

--- src/main/java/ru/yandex/practicum/filmorate/storage/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FeedStorage.java
@@ -4,6 +4,8 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface FeedStorage {
+
     void addEvent(Long userId, String eventType, String operation, Long entityId);
+
     List<Event> getFeed(Long userId);
 }
