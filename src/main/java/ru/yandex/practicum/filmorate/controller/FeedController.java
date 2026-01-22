24ed3904c3800b00836b807e5e61a47650e69227
Evgeniy <marchinskiye22@mail.ru>
2026-01-22 14:54:14 +0700

    recommendations

--- src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FeedController.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.Event;
-import ru.yandex.practicum.filmorate.service.EventService;
 
 import java.util.List;
 
